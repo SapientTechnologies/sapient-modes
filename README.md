2718355ead869c69d5c3763836527847d4fad83e
damian <damian@penguin>
2020-09-03 17:57:43 -0400

    initial code commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# sapient-modes
+
+Nuxt.js/Typescript Theme switcher with active content module.
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+For detailed explanation on the Nuxt Color Mode module check out [Going dark with Nuxt.js color mode](https://nuxtjs.org/blog/going-dark-with-nuxtjs-color-mode/).
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container">
+    <div>
+      <div class="color-picker-container">
+        <ColorModePicker />
+      </div>
+      <h1 class="title">
+        Sapient Modes
+      </h1>
+      <article>
+        <h1>{{ page.title }}</h1>
+        <nuxt-content :document="page" />
+      </article>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData ({ $content }) {
+    const page = await $content('hello').fetch()
+
+    return {
+      page
+    }
+  }
+})
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.title {
+    font-family:
+        'Quicksand',
+        'Source Sans Pro',
+        -apple-system,
+        BlinkMacSystemFont,
+        'Segoe UI',
+        Roboto,
+        'Helvetica Neue',
+        Arial,
+        sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+    text-align: center;
+    padding-bottom: 10px;
+}
+
+.subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+}
+
+.article {
+    text-align: left;
+}
+
+.color-picker-container {
+  text-align: center;
+  padding: 50px;
+}
+</style>
